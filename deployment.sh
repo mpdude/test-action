67840969ecd24d5c0035e462579c270965a25e8c
Matthias Pigulla <mp@webfactory.de>
2020-06-04 08:13:34 +0000

    Ermittle Repo und Commit über API

--- deployment.sh
+++ deployment.sh
@@ -1,7 +1,8 @@
 #!/bin/sh -ex
 
+export AWS_DEFAULT_REGION=`curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region`
+read -r REPOSITORY_NAME COMMIT_ID <<< $(aws deploy get-deployment --deployment-id=$DEPLOYMENT_ID | jq -r '.deploymentInfo.revision.gitHubLocation.repository + " " + .deploymentInfo.revision.gitHubLocation.commitId')
+export REPOSITORY_NAME COMMIT_ID 
 who am i
 pwd
 printenv
-
-aws deploy get-deployment --deployment-id=$DEPLOYMENT_ID | jq .
