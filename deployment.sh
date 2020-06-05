0cb3c0d3774f9f332f52fa76b567602a400add87
Matthias Pigulla <mp@webfactory.de>
2020-06-05 22:07:13 +0000

    Starte die Anwendung

--- deployment.sh
+++ deployment.sh
@@ -1,7 +1,6 @@
 #!/bin/bash -e
 
-export DEPLOYMENT_DIR=$(dirname $(readlink -f $0))
-echo "Deployment in $DEPLOYMENT_DIR"
+export DEPLOYMENT_DIR=$(dirname $(readlink -f $0))/..
 
 function BeforeInstall() {
     export AWS_DEFAULT_REGION=`curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region`
@@ -9,7 +8,7 @@ function BeforeInstall() {
     export REPO_URL COMMIT_ID
 
     export RELEASE=`date +%Y%m%d%H%M%S`
-    echo $RELEASE > $DEPLOYMENT_DIR/../depp-release-version
+    echo $RELEASE > $DEPLOYMENT_DIR/depp-release-version
 
     eval `ssh-agent -s`
     trap "echo Killing SSH agent with PID $SSH_AGENT_PID; kill $SSH_AGENT_PID" 0
@@ -25,10 +24,14 @@ function BeforeInstall() {
 
 }
 
+function ApplicationStart() {
+    cd /var/www/$DEPLOYMENT_GROUP_NAME
+    [ -L current_version ] && basename $(readlink -f current_version) > $DEPLOYMENT_DIR/depp-previous-release
+    depp deploy $(cat $DEPLOYMENT_DIR/depp-release-version)
+}
+
 function ValidateService() {
-    sleep 30
-    echo "Das wäre wohl ein Fehlschlag..."
-    exit 1
+  true
 }
 
 case $LIFECYCLE_EVENT in
@@ -41,6 +44,7 @@ case $LIFECYCLE_EVENT in
     ;;
 
     ApplicationStart)
+        ApplicationStart
     ;;
 
     ValidateService)
