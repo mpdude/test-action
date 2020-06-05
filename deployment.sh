16b5afaa1c58cc832520e824317352c215c1c32e
Matthias Pigulla <mp@webfactory.de>
2020-06-05 16:30:23 +0000

    Ein Test

--- deployment.sh
+++ deployment.sh
@@ -1,8 +1,5 @@
 #!/bin/sh -ex
 
 export AWS_DEFAULT_REGION=`curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region`
-read -r REPOSITORY_NAME COMMIT_ID <<< $(aws deploy get-deployment --deployment-id=$DEPLOYMENT_ID | jq -r '.deploymentInfo.revision.gitHubLocation.repository + " " + .deploymentInfo.revision.gitHubLocation.commitId')
-export REPOSITORY_NAME COMMIT_ID 
-who am i
-pwd
-printenv
+read -r REPOSITORY_NAME COMMIT_ID <<< $(aws deploy get-deployment --deployment-id=$DEPLOYMENT_ID | jq -r '"git@github.com:\(.deploymentInfo.revision.gitHubLocation.repository).git " + .deploymentInfo.revision.gitHubLocation.commitId')
+export REPOSITORY_NAME COMMIT_ID
