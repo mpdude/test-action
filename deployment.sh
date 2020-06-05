6a518d93d38dafa46478632e592f812b74bdb74a
Matthias Pigulla <mp@webfactory.de>
2020-06-05 16:55:46 +0000

    Export release

--- deployment.sh
+++ deployment.sh
@@ -3,6 +3,7 @@
 export AWS_DEFAULT_REGION=`curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region`
 read -r REPO_URL COMMIT_ID <<< $(aws deploy get-deployment --deployment-id=$DEPLOYMENT_ID | jq -r '"git@github.com:\(.deploymentInfo.revision.gitHubLocation.repository).git " + .deploymentInfo.revision.gitHubLocation.commitId')
 export REPO_URL COMMIT_ID
+export RELEASE=`date +%Y%m%d%H%M%S`
 
 cd /var/www
 if [ -d $DEPLOYMENT_GROUP ]; then
