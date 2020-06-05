d94eb65629201c55ae19d858f5586241c2fa4a59
Matthias Pigulla <mp@webfactory.de>
2020-06-05 17:49:56 +0000

    Jetzt depp

--- deployment.sh
+++ deployment.sh
@@ -7,14 +7,13 @@ export RELEASE=`date +%Y%m%d%H%M%S`
 
 eval `ssh-agent -s`
 trap "echo Killing SSH agent with PID $SSH_AGENT_PID; kill $SSH_AGENT_PID" 0
-
-printenv
 ssh-add
 
 cd /var/www
 if [ -d $DEPLOYMENT_GROUP_NAME ]; then
     echo updating deployment TBD
 else
-    git clone "git@github.com:webfactory/symfony-webfactory-edition.git" "$DEPLOYMENT_GROUP_NAME"
-    # depp setup "$DEPLOYMENT_GROUP" "$REPO_URL" "$COMMIT_ID"
+    #git clone "git@github.com:webfactory/symfony-webfactory-edition.git" "$DEPLOYMENT_GROUP_NAME"
+    #depp setup "$DEPLOYMENT_GROUP" "$REPO_URL" "$COMMIT_ID"
+    depp setup "$DEPLOYMENT_GROUP_NAME" "git@github.com:webfactory/symfony-webfactory-edition.git" 72cf70f2f71cf88f1be008cf72df8d523f6ecc6f
 fi 
