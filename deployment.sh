e7560f1d69762f974ed6f853525d7ed0f4f00d8b
Matthias Pigulla <mp@webfactory.de>
2020-06-05 16:46:57 +0000

    Setup

--- deployment.sh
+++ deployment.sh
@@ -1,8 +1,12 @@
 #!/bin/bash -ex
 
 export AWS_DEFAULT_REGION=`curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region`
-read -r REPOSITORY_NAME COMMIT_ID <<< $(aws deploy get-deployment --deployment-id=$DEPLOYMENT_ID | jq -r '"git@github.com:\(.deploymentInfo.revision.gitHubLocation.repository).git " + .deploymentInfo.revision.gitHubLocation.commitId')
-export REPOSITORY_NAME COMMIT_ID
+read -r REPO_URL COMMIT_ID <<< $(aws deploy get-deployment --deployment-id=$DEPLOYMENT_ID | jq -r '"git@github.com:\(.deploymentInfo.revision.gitHubLocation.repository).git " + .deploymentInfo.revision.gitHubLocation.commitId')
+export REPO_URL COMMIT_ID
 
-printenv
-who am i
+cd /var/www
+if [ -d $DEPLOYMENT_GROUP ]; then
+    echo updating deployment TBD
+else
+    depp setup "$DEPLOYMENT_GROUP" "$REPO_URL" "$COMMIT_ID"
+fi 
