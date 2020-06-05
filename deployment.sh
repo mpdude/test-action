b5e0c23bead09f7c5ab0773dfb02f21970919b9f
Matthias Pigulla <mp@webfactory.de>
2020-06-05 16:39:48 +0000

    Run it

--- deployment.sh
+++ deployment.sh
@@ -3,3 +3,6 @@
 export AWS_DEFAULT_REGION=`curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region`
 read -r REPOSITORY_NAME COMMIT_ID <<< $(aws deploy get-deployment --deployment-id=$DEPLOYMENT_ID | jq -r '"git@github.com:\(.deploymentInfo.revision.gitHubLocation.repository).git " + .deploymentInfo.revision.gitHubLocation.commitId')
 export REPOSITORY_NAME COMMIT_ID
+
+printenv
+who am i
