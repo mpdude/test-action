1cee36d49749b16a1f6e0274870aa8998f003e59
Matthias Pigulla <mp@webfactory.de>
2020-06-07 18:36:59 +0000

    Vermeide exit-Codes im Skript (bash -ex!)

--- deployment.sh
+++ deployment.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -ex 
+#!/bin/bash -ex
 
 export DEPLOYMENT_DIR=$(dirname $(readlink -f $0))/..
 
@@ -6,10 +6,7 @@ function BeforeInstall() {
     export AWS_DEFAULT_REGION=`curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region`
     read -r REPO_URL COMMIT_ID <<< $(aws deploy get-deployment --deployment-id=$DEPLOYMENT_ID | jq -r '"git@github.com:\(.deploymentInfo.revision.gitHubLocation.repository).git " + .deploymentInfo.revision.gitHubLocation.commitId')
     export REPO_URL COMMIT_ID
-
-    # export RELEASE=`date +%Y%m%d%H%M%S`
     export RELEASE=$DEPLOYMENT_ID
-    #echo $RELEASE > $DEPLOYMENT_DIR/depp-release-version
 
     eval `ssh-agent -s`
     trap "echo Killing SSH agent with PID $SSH_AGENT_PID; kill $SSH_AGENT_PID" 0
@@ -24,7 +21,6 @@ function BeforeInstall() {
     else
         depp setup "$DEPLOYMENT_GROUP_NAME" "$REPO_URL" "$COMMIT_ID"
     fi
-
 }
 
 function ApplicationStop() {
@@ -41,23 +37,22 @@ function ApplicationStop() {
 
 function ApplicationStart() {
     cd /var/www/$DEPLOYMENT_GROUP_NAME
-    # [ -L current_version ] && basename $(readlink -f current_version) > $DEPLOYMENT_DIR/depp-previous-release
-    #depp deploy $(cat $DEPLOYMENT_DIR/depp-release-version)
     depp deploy $DEPLOYMENT_ID
-}
-
-function ValidateService() {
-  true
 }
 
 function AfterInstall() {
     cd /var/www/$DEPLOYMENT_GROUP_NAME/$DEPLOYMENT_ID
-    [ -x bin/console ] && bin/console list --raw | grep -q doctrine:migrations:migrate && bin/console doctrine:migrations:migrate --allow-no-migration --no-ansi --no-interaction
-    [ -f meta/wfdynamic.xml ] && phlough wfdynamic:configuration-import
+    if [ -x bin/console ]; then
+        if bin/console list --raw | grep -q doctrine:migrations:migrate; then
+            bin/console doctrine:migrations:migrate --allow-no-migration --no-ansi --no-interaction
+        fi
+    fi
+    if [ -f meta/wfdynamic.xml ]; then
+        phlough wfdynamic:configuration-import
+    fi
 }
 
 case $LIFECYCLE_EVENT in
-
     ApplicationStop)
         ApplicationStop
     ;;
@@ -73,9 +68,4 @@ case $LIFECYCLE_EVENT in
     ApplicationStart)
         ApplicationStart
     ;;
-
-    ValidateService)
-        ValidateService
-    ;;
-
 esac
